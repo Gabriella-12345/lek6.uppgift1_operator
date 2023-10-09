e62633808f3310a3355f569f3e57ea34f3d05daa
gabriella jonsson <gabriella.jonsson@elev.ga.ntig.se>
2023-10-09 08:56:47 +0200

    Skapa en scanner och en startfråga

--- src/App.java
+++ src/App.java
@@ -1,5 +1,12 @@
+ import java.util.Scanner;
 public class App {
     public static void main(String[] args) throws Exception {
+        Scanner tb = new Scanner(System.in);
         System.out.println("uppgift 1");
+    
+        System.out.println("Välkommen till systembolaget!" + "\n");
+        System.out.println("Hur gammmal är du?");
+
+
     }
 }
